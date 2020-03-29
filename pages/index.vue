d9701c33a19404da0dbed0ce6c9256081e7a599b
Amie Croteau <ac04400@ravens.racc.edu>
2020-03-29 18:00:20 -0400

    Adjusted Header image

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex>
+      <v-parallax
+        src="https://reformedforum.org/wp-content/blogs.dir/1/files/2015/02/book_pile.jpg"
+        gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+        dark
+      >
+        <v-row align="center" justify="center">
+          <v-col class="text-center" cols="12">
+            <h1 class="display-1 font-weight-thin mb-4">
+              COVID19 Student Resources
+            </h1>
+            <h4 class="subheading">Select the age group of your student</h4>
+          </v-col>
+        </v-row>
+      </v-parallax>
       <Stepper />
     </v-flex>
   </v-layout>
